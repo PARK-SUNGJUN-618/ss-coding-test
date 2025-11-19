c4e62a9915b9e0cf4eb29e55972f8fcd470571d8
Sungjun Park <espoir618@gmail.com>
2025-11-19 21:19:39 +0900

    dfs-2

---
+++ src/CodingTest2.java
@@ -0,0 +1,10 @@
+public class CodingTest2 {
+    public static void main(String[] args) {
+        // ex1)result:11
+        int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+        
+        // ex2)result:-1
+        //int[][] maps2 = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}};
+
+    }
+}
