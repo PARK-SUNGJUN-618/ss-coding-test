5cd147d989b68e4c2509d528cf8c0c78fcbc6821
Sungjun Park <espoir618@gmail.com>
2025-11-29 22:01:02 +0900

    dfs-2

--- src/CodingTest2.java
+++ src/CodingTest2.java
@@ -33,27 +33,23 @@ public class CodingTest2 {
         visited[n][m] = true;
         System.out.println("n:" + n + ",m:" + m);
 
-        if (n == 4 && m == 4) return;
+        if (n == maxN-1 && m == maxM-1) return;
 
         if (n < maxN-1 && !visited[n+1][m] && maps[n+1][m] == 1) {
-            n++;
             answer++;
-            dfs(maps, n, m);
+            dfs(maps, n+1, m);
         }
         if (m < maxM-1 && !visited[n][m+1] && maps[n][m+1] == 1) {
-            m++;
             answer++;
-            dfs(maps, n, m);
+            dfs(maps, n, m+1);
         }
         if (n > 0 && !visited[n-1][m] && maps[n-1][m] == 1) {
-            n--;
             answer++;
-            dfs(maps, n, m);
+            dfs(maps, n-1, m);
         }
         if (m > 0 && !visited[n][m-1] && maps[n][m-1] == 1) {
-            m--;
             answer++;
-            dfs(maps, n, m);
+            dfs(maps, n, m-1);
         }
 
     }
