b77e0bcf68627b516b8e0f88200622b0467a900f
Sungjun Park <espoir618@gmail.com>
2025-12-06 20:07:15 +0900

    dfs-3

--- src/CodingTest3.java
+++ src/CodingTest3.java
@@ -1,5 +1,34 @@
 public class CodingTest3 {
+
+    static boolean graph[][];
+    static boolean visited[][];
+    static int n;
+    static int result;
+    static int resultCnts[];
     public static void main(String[] args) {
-        System.out.println("start");
+        int in_n = 7;
+        boolean[][] in_graph = new boolean[][]{
+            {false,true ,true ,false,true ,false,false},
+            {false,true ,true ,false,true ,false,true },
+            {true ,true ,true ,false,true ,false,true },
+            {false,false,false,false,true ,true ,true },
+            {false,true ,false,false,false,false,false},
+            {false,true ,true ,true ,true ,true ,false},
+            {false,true ,true ,true ,false,false,false}
+        };
+        
+        solution(n, graph);
+        
+        System.out.println(result);
+        for(int resultCnt : resultCnts) {
+            System.out.println(resultCnt);
+        }
+    }
+    private static void solution(int in_n, boolean[][] in_graph) {
+        n = in_n;
+        graph = in_graph;
+        visited = new boolean[n][n];
+
+        dfs();
     }
 }
