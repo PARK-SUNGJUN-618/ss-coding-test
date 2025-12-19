b64b9c0e6e8d58da8499a2ecaf875c201590fff3
Sungjun Park <espoir618@gmail.com>
2025-12-19 19:23:34 +0900

    5-dfs

--- src/CodingTest5.java
+++ src/CodingTest5.java
@@ -1,5 +1,20 @@
 public class CodingTest5 {
+
+    /*
+     * Find the prime numbers.
+     *
+     * Problem:
+     * Generate all possible numbers from the given digit pieces
+     * (using 1 ~ N digits) and count the number of unique prime numbers.
+     *
+     * Example:
+     * Input : "17"
+     * Output: 3 (7, 17, 71)
+     *
+     * @param numbers String containing digit pieces (e.g. "011")
+     * @return Number of unique prime numbers
+     */
     public static void main(String[] args) {
-        
+
     }
 }
